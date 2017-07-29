55461b9fce749ea902a0d220403630118ff9bd50
Ryan Tsang <ryantsangai@gmail.com>
2017-07-29 19:45:24 +0800

    move api to apis dir

--- main.py
+++ main.py
@@ -3,32 +3,10 @@
 
 import endpoints
 
-from protorpc import message_types
-from protorpc import messages
-from protorpc import remote
+from apis.demo import DemoApi
 
 import logging
 
-
-class DemoRequest(messages.Message):
-    message = messages.StringField(1, required=True)
-
-
-class DemoResponse(messages.Message):
-    word_count = messages.IntegerField(1, required=True)
-    message = messages.StringField(2)
-
-
-@endpoints.api(name='demo', version='v1')
-class DemoApi(remote.Service):
-
-    @endpoints.method(DemoRequest, DemoResponse, path="demo")
-    def count_words(self, request):
-        message = request.message
-        result = DemoResponse()
-        result.word_count = len(message)
-        result.message = message
-        return result
 
 
 api = endpoints.api_server([DemoApi])
